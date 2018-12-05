62dd3f63e651b00ebd8e8ffa7e42f190ff3209a3
tao.dai <tao.dai@wehotelglobal.com>
2018-12-05 11:16:59 +0800

    Kotlin ?

--- src/com/dt/learn/classObject/User.kt
+++ src/com/dt/learn/classObject/User.kt
@@ -4,4 +4,12 @@ package com.dt.learn.classObject
  * @author dt 2018/11/27 18:34
  * 数据类
  */
-data class User(val name: String, val age: Int)
+data class User(val name: String?, val age: Int)
+
+fun main(args: Array<String>) {
+
+    val user: User? = null
+
+    println(user?.name.isNullOrEmpty())
+
+}
